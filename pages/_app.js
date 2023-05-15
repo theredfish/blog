64947ce0eb1584c8703c6a9d01d40b9dd1acb9a0
theredfish <did.julian@gmail.com>
2023-05-15 17:20:25 +0200

    Add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import "../styles/global.css";
 import "prismjs/themes/prism-twilight.min.css";
+import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="theredfi.sh">
+      <Component {...pageProps} />;
+    </PlausibleProvider>
+  );
 }
